00e5da55c3d67bc6481bf021992aa948097a97ab
Khairul Hossain Saikat <khsaikat000@gmail.com>
2023-02-18 19:09:32 +0100

    3nd commit!!
    changed to postgresql database... added AWS RDS for db server

--- World_of_Khairul/settings.py
+++ World_of_Khairul/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'World_of_Khairul.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'world_of_khairul_db',
+        'USER': 'khsaikat',
+        'PASSWORD': '1!DipuNo.21!',
+        'HOST': 'pgresql-inst-1.cgdchjrx2kdq.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
